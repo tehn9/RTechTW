63b94ce22bbbec5214c3c78c6d18e88b89505a1c
tehn9 <vladiconst@yahoo.com>
2017-12-05 01:06:50 +0200

    JS - resize header text function adjust
    css - credits adjustments
    
    html - added id to pre

--- scripts.js
+++ scripts.js
@@ -3,3 +3,45 @@ function src(page){
   document.getElementById('frame').src = page + ".html";
   return null;
 }
+/*
+function adjust()
+{
+//  var size;
+    while(document.getElementById('pre').scrollWidth > document.getElementById('pre').clientWidth){
+
+    //    size = window.getComputedStyle(document.getElementById('pre')).fontSize;
+    //    size--;
+      //  document.getElementById('pre').style.fontSize = size;
+      document.getElementById('pre').style.fontSize = parseInt(window.getComputedStyle(document.getElementById("pre")).fontSize) -1;
+     }
+     while(document.getElementById('pre').scrollWidth == document.getElementById('pre').clientWidth)
+     {
+       document.getElementById('pre').style.fontSize = parseInt(window.getComputedStyle(document.getElementById("pre")).fontSize) +1;
+       if(document.getElementById('pre').scrollWidth > document.getElementById('pre').clientWidth)
+       {
+         document.getElementById('pre').style.fontSize = parseInt(window.getComputedStyle(document.getElementById("pre")).fontSize) -1;
+         break;
+       }
+     }
+
+}
+*/
+function adjust(obj)
+{
+//  var size;
+    if(document.getElementById(obj).scrollWidth > document.getElementById(obj).clientWidth)
+    {
+      document.getElementById(obj).style.fontSize = parseInt(window.getComputedStyle(document.getElementById(obj)).fontSize) -1;
+    }
+
+    if(document.getElementById(obj).scrollWidth == document.getElementById(obj).clientWidth)
+    {
+      document.getElementById(obj).style.fontSize = parseInt(window.getComputedStyle(document.getElementById(obj)).fontSize) + 1;
+    }
+
+    if(document.getElementById(obj).scrollWidth > document.getElementById(obj).clientWidth)
+    {
+      document.getElementById(obj).style.fontSize = parseInt(window.getComputedStyle(document.getElementById(obj)).fontSize) -1;
+    }
+
+}

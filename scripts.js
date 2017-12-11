dcb204c7b3d18f3e4fade3a035e1e7d11194e904
tehn9 <vladiconst@yahoo.com>
2017-12-11 21:26:05 +0200

    cosmetic changes forget iframes

--- scripts.js
+++ scripts.js
@@ -3,29 +3,30 @@ function src(page){
   document.getElementById('frame').src = page + ".html";
   return null;
 }
-/*
-function adjust()
+
+function adjust(obj)
 {
 //  var size;
-    while(document.getElementById('pre').scrollWidth > document.getElementById('pre').clientWidth){
+    while(document.getElementById(obj).scrollWidth > document.getElementById(obj).clientWidth){
 
     //    size = window.getComputedStyle(document.getElementById('pre')).fontSize;
     //    size--;
       //  document.getElementById('pre').style.fontSize = size;
-      document.getElementById('pre').style.fontSize = parseInt(window.getComputedStyle(document.getElementById("pre")).fontSize) -1;
+      document.getElementById(obj).style.fontSize = parseInt(window.getComputedStyle(document.getElementById(obj)).fontSize) -1;
      }
-     while(document.getElementById('pre').scrollWidth == document.getElementById('pre').clientWidth)
+     while(document.getElementById(obj).scrollWidth == document.getElementById(obj).clientWidth)
      {
-       document.getElementById('pre').style.fontSize = parseInt(window.getComputedStyle(document.getElementById("pre")).fontSize) +1;
-       if(document.getElementById('pre').scrollWidth > document.getElementById('pre').clientWidth)
+       document.getElementById(obj).style.fontSize = parseInt(window.getComputedStyle(document.getElementById(obj)).fontSize) +1;
+       if(document.getElementById(obj).scrollWidth > document.getElementById(obj).clientWidth)
        {
-         document.getElementById('pre').style.fontSize = parseInt(window.getComputedStyle(document.getElementById("pre")).fontSize) -1;
+         document.getElementById(obj).style.fontSize = parseInt(window.getComputedStyle(document.getElementById(obj)).fontSize) -1;
          break;
        }
      }
 
 }
-*/
+
+/*
 function adjust(obj)
 {
 //  var size;
@@ -45,3 +46,4 @@ function adjust(obj)
     }
 
 }
+*/
